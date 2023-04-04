e329d93f6d035af7994470f2a299e1b6136ecc35
Edy Molina <36119589+edymol@users.noreply.github.com>
2023-04-04 14:44:35 -0700

    feat: add create component

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
 const create = async () => {
-    // Write your code here 
+  const fs = require('fs');
+  const path = require('path');
+
+  const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
+  try {
+    await fs.promises.access(filePath);
+    throw new Error('FS operation failed: file already exists');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fs.promises.writeFile(filePath, 'I am fresh and young');
+      console.log('File created successfully!');
+    } else {
+      throw error;
+    }
+  }
 };
 
 await create();
